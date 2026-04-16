4f2b68346335d8723a4af30e46b134c3f4829ca7
renllan <brightlan5288@gmail.com>
2026-04-15 20:48:18 -0700

    fix: pre-compile TypeScript to eliminate Lambda cold start timeout

--- lambda.js
+++ lambda.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
-require('ts-node/register');
 
 const serverlessExpress = require('@vendia/serverless-express');
-const app = require('./app').default;
+const app = require('./dist/app').default;
 
 // Wrap Express app as a Lambda handler
 exports.handler = serverlessExpress({ app });
