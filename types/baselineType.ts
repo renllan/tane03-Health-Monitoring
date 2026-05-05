65e183804de91b309e0e37eaea5fac125d0dbcb7
renllan <brightlan5288@gmail.com>
2026-05-04 22:01:48 -0700

    udpated baseline types

--- types/baselineType.ts
+++ types/baselineType.ts
@@ -6,6 +6,7 @@ export enum BaselineType {
     Bloodpressure = "Bloodpressure",
     SleepDuration = "SleepDuration",
     SleepScore = "SleepScore",
+    SleepAvgHR = "SleepAvgHeartRate",
     RHR = "RHR",
 
 }
