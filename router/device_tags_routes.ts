209b828950667a129f3a7c1052ab07ec8d92c984
renllan <brightlan5288@gmail.com>
2026-06-15 11:39:26 +0800

    udpate route name to dinstiguish bewteen querying by group, imei, tag

--- router/device_tags_routes.ts
+++ router/device_tags_routes.ts
@@ -4,9 +4,9 @@ import { DeviceTagController } from '../controller/deviceTagController';
 const router = Router();
 const deviceTagController = new DeviceTagController();
 
-router.get('/:imei', (req, res) => { deviceTagController.getTagsByImei(req, res); });
+router.get('/imei/:imei', (req, res) => { deviceTagController.getTagsByImei(req, res); });
 router.get('/query/:tag', (req, res) => { deviceTagController.queryByTag(req, res); });
-router.get('/:group_id', (req, res) => { deviceTagController.getByGroupId(req, res); });
+router.get('/group/:group_id', (req, res) => { deviceTagController.getByGroupId(req, res); });
 router.post('/add', (req, res) => { deviceTagController.addEntry(req, res); });
 router.delete('/delete/:tag', (req, res) => { deviceTagController.deleteTag(req, res); });
 router.delete('/delete/:imei/:tag', (req, res) => { deviceTagController.deleteTagFromDevice(req, res); });
